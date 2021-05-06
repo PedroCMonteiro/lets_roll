ed3b92ea23bde489ac8df3b39595c6d037658942
Elaine <elaine.santos@m4u.com.br>
2021-05-05 23:30:28 -0300

    cores

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,16 @@ import 'models/Pages/MainCategoriesPage.dart';
 import 'models/Pages/SubCategoriesPage.dart';
 
 Map<int, Color> color = {
-  50: Color.fromRGBO(71, 36, 4, .1),
-  100: Color.fromRGBO(71, 36, 4, .2),
-  200: Color.fromRGBO(71, 36, 4, .3),
-  300: Color.fromRGBO(71, 36, 4, .4),
-  400: Color.fromRGBO(71, 36, 4, .5),
-  500: Color.fromRGBO(71, 36, 4, .6),
-  600: Color.fromRGBO(71, 36, 4, .7),
-  700: Color.fromRGBO(71, 36, 4, .8),
-  800: Color.fromRGBO(71, 36, 4, .9),
-  900: Color.fromRGBO(71, 36, 4, 1),
+  50: Color.fromRGBO(30,34,97, .1),
+  100: Color.fromRGBO(30,34,97, .2),
+  200: Color.fromRGBO(30,34,97, .3),
+  300: Color.fromRGBO(30,34,97, .4),
+  400: Color.fromRGBO(30,34,97, .5),
+  500: Color.fromRGBO(30,34,97, .6),
+  600: Color.fromRGBO(30,34,97, .7),
+  700: Color.fromRGBO(30,34,97, .8),
+  800: Color.fromRGBO(30,34,97, .9),
+  900: Color.fromRGBO(30,34,97, 1),
 };
 MaterialColor colorCustom = MaterialColor(0xFF472404, color);
 
@@ -28,15 +28,11 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Let\'s Roll',
       theme: ThemeData(
-        primarySwatch: colorCustom,
-        primaryColor: colorCustom,
-        accentColor: colorCustom,
-        //  fontFamily: 'Georgia',
-        //  textTheme: TextTheme(
-        //     headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-        //     headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
-        //     bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
-        //   ),
+        brightness: Brightness.dark,
+        backgroundColor: colorCustom[50],
+        primarySwatch: colorCustom[300],
+        primaryColor: colorCustom[600],
+        accentColor: colorCustom[900],
       ),
       routes: {
         '/': (context) => LoginPage(),
